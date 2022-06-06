24563191a49866e3fa30b7f570a2d56d59ff0041
unknown <md616264@gmail.com>
2022-06-07 00:01:15 +0600

    finish task -2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home/Home';
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Home></Home>
+      <Services></Services>
     </div>
   );
 }
---
+++ src/components/Services/Services.js
@@ -0,0 +1,53 @@
+
+
+
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='grid grid-cols-1 gap-5 md:grid-cols-4'>
+            <div >
+                <div class="card h-[600px] bg-base-100 shadow-xl">
+                    <figure><img src="https://img.freepik.com/free-vector/teamwork-concept-landing-page_52683-20165.jpg?size=338&ext=jpg&ga=GA1.2.2090978668.1652901003" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="card h-[600px] bg-base-100 shadow-xl">
+                    <figure><img src="https://img.freepik.com/free-vector/people-icon-collection_1408-10.jpg?size=338&ext=jpg&ga=GA1.2.2090978668.1652901003" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="card h-[600px] bg-base-100 shadow-xl">
+                    <figure><img src="https://img.freepik.com/free-vector/business-people-with-connection-icons_53876-66205.jpg?size=626&ext=jpg&ga=GA1.2.2090978668.1652901003" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="card h-[600px] bg-base-100 shadow-xl">
+                    <figure><img src="https://img.freepik.com/free-vector/business-people-with-connection-icons_53876-66204.jpg?size=626&ext=jpg&ga=GA1.2.2090978668.1652901003" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
